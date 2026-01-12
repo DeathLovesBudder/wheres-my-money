302ae2932c0410cabbc942920bf02257a3676afd
DeathLovesBudder <jhardean13@gmail.com>
2026-01-12 13:03:07 +0200

    restart

--- server/server.js
+++ server/server.js
@@ -1,26 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import mongoose from 'mongoose';
-
-dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'OK' });
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
 });
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch(console.error);
